31ed9490f04e823dcbdff5573c9c41efa48bbafd
Mehul <mehul@triamsecurity.com>
2025-02-12 13:12:05 +0530

    update

--- test.py
+++ test.py
@@ -6,3 +6,14 @@ elif age>=18 and age<=30:
     print("You are a teenager");
 elif age>30 and age<50:
     print("You are a adult");
+if age<18:
+    print("You are a child");
+elif age>=18 and age<=30:
+    print("You are a teenager");
+elif age>30 and age<50:
+    print("You are a adult");if age<18:
+    print("You are a child");
+elif age>=18 and age<=30:
+    print("You are a teenager");
+elif age>30 and age<50:
+    print("You are a adult");
