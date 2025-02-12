bc8225e5033f6e135e0733609aa6db0ba1b2780d
Mehul <mehul@triamsecurity.com>
2025-02-12 13:15:13 +0530

    bugfix

--- test.py
+++ test.py
@@ -1,19 +1,2 @@
 age = int(input("Enter your Age:"));
 
-if age<18:
-    print("You are a child");
-elif age>=18 and age<=30:
-    print("You are a teenager");
-elif age>30 and age<50:
-    print("You are a adult");
-if age<18:
-    print("You are a child");
-elif age>=18 and age<=30:
-    print("You are a teenager");
-elif age>30 and age<50:
-    print("You are a adult");if age<18:
-    print("You are a child");
-elif age>=18 and age<=30:
-    print("You are a teenager");
-elif age>30 and age<50:
-    print("You are a adult");
